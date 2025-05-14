ae7d83085ed213fa838edf2818376a476421b73c
Jingze <67879550+YoYo-JDCE@users.noreply.github.com>
2025-05-14 15:32:43 -0400

    Add files via upload

---
+++ Analyzing the Policies and Uncertainty-Extension Strawberries/MulchpaperExtension.m
@@ -0,0 +1,62 @@
+%% This section discusses the impact of the current certification scheme
+% We compare the life-time profit under two different scenarios when a farmer
+% disposes of all plastic waste and when a farmer disposes of none of the
+% plastic waste
+%% When a farmer disposes of none of the plastic waste under PEM when price=$1/lb
+z0policy0
+%% When a farmer disposes of none of the plastic waste under BDM when price=$1/lb
+z0policy90
+%% When a farmer disposes of all plastic waste under PEM when price=$1/lb
+z1policy0
+%% When a farmer disposes of all plastic waste under BDM when price=$1/lb
+z1policy90
+
+%% When a farmer disposes of none of the plastic waste under PEM when price=$2/lb
+z0policy0P2
+%% When a farmer disposes of none of the plastic waste under BDM when price=$2/lb
+z0policy90P2
+%% When a farmer disposes of all plastic waste under PEM when price=$2/lb
+Z1policy0P2
+%% When a farmer disposes of all plastic waste under BDM when price=$2/lb
+Z1policy90P2
+
+%% When a farmer disposes of none of the plastic waste under PEM when price=$3/lb
+z0policy0P3
+%% When a farmer disposes of none of the plastic waste under BDM when price=$3/lb
+z0policy90P3
+%% When a farmer disposes of all plastic waste under PEM when price=$3/lb
+Z1policy0P3
+%% When a farmer disposes of all plastic waste under BDM when price=$3/lb
+Z1policy90P3
+
+
+%% When a farmer disposes of none of the plastic waste under PEM when price=$3/lb and Large initial plastic pollutant
+z0policy0P3LargeInitialPollutant
+%% When a farmer disposes of none of the plastic waste under BDM when price=$3/lb and Large initial plastic pollutant
+z0policy90P3LargeInitialPollutant
+%% When a farmer disposes of all plastic waste under PEM when price=$3/lb and Large initial plastic pollutant
+Z1policy0P3LargeInitialPollutant
+%% When a farmer disposes of all plastic waste under BDM when price=$3/lb and Large initial plastic pollutant
+Z1policy90P3LargeInitialPollutant
+
+
+%% This section discusses Pollution Accumulation in a Stochastic Environment
+% We assume the random shocks follow a normal distribution with mean equal
+% to mu and the standard deviation sigma
+%% When a farmer disposes of none of the plastic waste and mu is 0, Sigma is 0.1
+FarDisNoneMu0Sig01
+%% When a farmer disposes of none of the plastic waste and mu is 0, Sigma is 0.2
+FarDisNoneMu0Sig02
+%% When a farmer disposes of none of the plastic waste and mu is 0.01, Sigma is 0.1
+FarDisNoneMu001Sig01
+%% When a farmer disposes of none of the plastic waste and mu is 0.3, Sigma is 0.1
+FarDisNoneMu03Sig01
+%% When a farmer disposes of all plastic waste and mu is 0, Sigma is 0.1
+FarDisMu0Sig01
+%% When a farmer disposes of all plastic waste and mu is 0, Sigma is 0.2
+FarDisMu0Sig02
+%% When a farmer disposes of all plastic waste and mu is 0.01, Sigma is 0.1
+FarDisMu001Sig01
+%% When a farmer disposes of all plastic waste and mu is 0.3, Sigma is 0.1
+FarDisMu03Sig01
+
